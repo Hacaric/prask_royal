80bd0daa3b92369ba5255e1a5ca774ff1f81f08e
hacaric <hacaric.dev@gmail.com>
2025-11-23 16:38:11 +0100

    Added tiles

--- server.py
+++ server.py
@@ -13,10 +13,14 @@ class Map:
     def __init__(self, width = Settings.Map.width, height = Settings.Map.height):
         self.width = width
         self.height = height
+        self.map = [[[] for __ in range(height)] for _ in range(width)]
+        self.map_structures = [[[] for __ in range(height)] for _ in range(width)]
         self.structures = []
         for tower in Settings.Map.Structures.towers:
             self.structures.append(Stucture(tower["type"], tower["x"], tower["y"]))
-            self.structures.append(Stucture(tower["type"], Settings.Map.height-tower["x"], tower["y"]))
+            self.map_structures[tower["x"]][tower["y"]] = len(self.structures) - 1
+            self.structures.append(Stucture(tower["type"], tower["x"], Settings.Map.height-tower["y"]))
+            self.map_structures[tower["x"]][Settings.Map.height-tower["y"]] = len(self.structures) - 1
 class Game:
     def __init__(self, map_:Map):
         self.map = map_
